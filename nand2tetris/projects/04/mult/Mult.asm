310855eefe4f1413c5182ef1bb233fd9ff6226a1
Seb W <thelayeredmind@gmail.com>
2022-05-04 09:30:39 +0200

    Implement Multiply in machine language

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,42 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+@R1
+D=M
+
+@mul
+M=D
+
+@R0
+D=M
+
+@R2
+M=0
+
+@END
+D;JEQ
+
+(MULTIPLY)
+  @mul
+  D=M
+  
+  @END
+  D;JEQ 
+
+  @R0
+  D=M
+
+  @R2
+  M=D+M
+
+  @mul
+  M = M-1
+  
+  @MULTIPLY
+  0;JMP 
+
+(END)
+  @END
+  0;JMP 
