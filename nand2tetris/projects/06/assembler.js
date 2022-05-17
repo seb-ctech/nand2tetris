e33b1a5934e9826be885fb3f0b5769b69b746ac6
Seb W <thelayeredmind@gmail.com>
2022-05-17 16:11:08 +0200

    Hack Assembler: Remove white spaces and commented lines

--- nand2tetris/projects/06/assembler.js
+++ nand2tetris/projects/06/assembler.js
@@ -21,14 +21,62 @@ const programs = [
   "./rect/RectL.asm"
 ]
 
-testPrograms(["./max/MaxL.asm"]);
+const C = {
+  op:
+    {
+      "0": "101010",
+      "1": "111111",
+      "-1": "111010",
+      "D": "001100",
+      "X": "110000",
+      "!D": "001101",
+      "!X": "110001",
+      "-D": "001111",
+      "-X": "110011",
+      "D+1": "011111",
+      "X+1": "110111",
+      "D-1": "001110",
+      "X-1": "110010",
+      "D+X": "000010",
+      "D-X": "010011",
+      "X-D": "000111",
+      "D&X": "000000",
+      "D|X": "010101"
+    }
+  ,
+  dest: {
+    M: 3,
+    D: 2,
+    A: 1
+  },
+  jump: {
+    n: "000",
+    JGT: "001",
+    JEQ: "010",
+    JGE: "011",
+    JLT: "100",
+    JNE: "101",
+    JLE: "110",
+    JMP: "111"
+  }
+}
+
+testInstructions = [
+  "@101",
+  "(LOOP)",
+  "@LOOP",
+  "D = A+1; JLT",
+  "A=D-1; // Some Comment and D=M+1 code",
+  "0;JMP",
+  "//Comment to eliminate"
+]
 
 function assemble(code){
   return someBinary;
 }
 
 
-function testPrograms(programs){
+function assemblePrograms(programs){
   programs.map(program => ({src: program, bin: assemble(readInputFile(program))}))
     .forEach(binary => writeBinaryFile(binary.bin, path.format({
       dir: "./",
@@ -43,6 +91,15 @@ function readInputFile(path){
   return lines;
 }
 
+function removeComments(lines){
+  const ruleFindComments = new RegExp("\/\/.*", 'g');
+  const ruleCommentedLine = new RegExp("^\/\/.*", 'g');
+  const ruleFindEmptyLines = new RegExp("^\s*$", 'g');
+  return lines.filter(line => !ruleFindEmptyLines.test(line))
+    .map(line => line.replace(/\s/g, ""))
+    .filter(line => !ruleCommentedLine.test(line))
+    .map(line => line.replace(ruleFindComments, ""))
+}
 
 function writeBinaryFile(code, path){
   fs.writeFile(path, code.join("\n"), console.error);
@@ -52,9 +109,20 @@ function writeBinaryFile(code, path){
 
 // Parsing
 
+
 // Code Translation
+function identifyOpCode(){
+
+}
+
 
 
 // Symbol Table Handling
 
-// Main Process
+function resolveSymbols(){
+
+}
+
+function assignLabels(){
+
+}
