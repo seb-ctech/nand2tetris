4a382c2b3bfa6fce975b0d2020f3101079014aa3
Seb W <thelayeredmind@gmail.com>
2022-11-02 10:54:37 +0100

    Project 7: Implement Simple push writer

---
+++ nand2tetris/projects/07/MemoryAccess/BasicTest/BasicTest.asm
@@ -0,0 +1,149 @@
+// push constant 10
+@10
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop local 0
+@SP
+M=M-1
+@0
+D=A
+// push constant 21
+@21
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 22
+@22
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop argument 2
+@SP
+M=M-1
+@2
+D=A
+// pop argument 1
+@SP
+M=M-1
+@1
+D=A
+// push constant 36
+@36
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop this 6
+@SP
+M=M-1
+@6
+D=A
+// push constant 42
+@42
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 45
+@45
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop that 5
+@SP
+M=M-1
+@5
+D=A
+// pop that 2
+@SP
+M=M-1
+@2
+D=A
+// push constant 510
+@510
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop temp 6
+@SP
+M=M-1
+@6
+D=A
+// push local 0
+@0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push that 5
+@5
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+NOPE
+// push argument 1
+@1
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+NOPE
+// push this 6
+@6
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push this 6
+@6
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+NOPE
+// sub
+NOPE
+// push temp 6
+@6
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+NOPE
--- nand2tetris/projects/07/vmtranslator.js
+++ nand2tetris/projects/07/vmtranslator.js
@@ -4,6 +4,14 @@ const path = require('path')
 
 const input_file = process.argv[2];
 
+const symbolMap = {
+  sp: "SP",
+  local: "LCL",
+  this: "THIS",
+  that: "THAT",
+  argument: "ARG"
+}
+
 translate(input_file);
 
 function translate(input_file){
@@ -20,8 +28,43 @@ function parse(line){
 }
 
 function writer(command){
-  //TODO: Implement Writer
-  return command.command
+  const original = "// " + Object.values(command).join(" ");
+  const assembly = writeAssembly(command);
+  const block = original + "\n" + assembly
+  return block
+}
+
+// TODO: Return a closure that recursively goes over all commands?
+function writeAssembly(command){
+  const combineLines = lines => lines.join("\n")
+  const commandOnLocation = (command, followUp) => {
+    const symbol = symbolMap[command.location]
+    const isConstant = symbol == null
+    const resolveLocation = ["@" + (isConstant ? command.value : symbol), "D=A"]
+    return combineLines(isConstant ? resolveLocation : resolveLocation.concat(["@" + command.value, "A=D+A"], followUp))
+  }
+  const stackPointer = combineLines(["@SP", "A=M"])
+  const mem = {read: "D=M", write: "M=D"}
+  const stackChange = offset => combineLines(["@SP", "M=M" + offset])
+  const commandTranslator = {
+    "pop": command => combineLines(
+        [
+          stackChange("-1"),
+          commandOnLocation(command),
+        ]
+      ),
+    "push": command => combineLines(
+        [
+          commandOnLocation(command, [mem.read]),
+          stackPointer,
+          mem.write,
+          stackChange("+1")
+        ]
+      ),
+    "add": command => "NOPE",
+    "sub": command => "NOPE"
+  }
+  return commandTranslator[command.command](command)
 }
 
 function removeComments(lines){
